0ac42237dcbd5a64eebf91b4175265651fb8e89a
rryanhermes <106575167+rryanhermes@users.noreply.github.com>
2023-03-08 18:51:16 -0600

    testing this out

--- options.py
+++ options.py
@@ -4,23 +4,24 @@ from tda import auth
 import json
 import pprint
 
-try:
-    key = auth.client_from_token_file(config.token_path, config.api_key)
-except FileNotFoundError:
-    from selenium import webdriver
-    with webdriver.Chrome(executable_path='/Users/ryanhermes/opt/anaconda3/envs/td-bot/chromedriver') as driver:
-        key = auth.client_from_login_flow(driver, config.api_key, config.redirect_uri, config.token_path)
+def authenticate():
+    try:
+        return auth.client_from_token_file(config.token_path, config.api_key)
+    except FileNotFoundError:
+        from selenium import webdriver
+        with webdriver.Chrome(executable_path='/Users/ryanhermes/opt/anaconda3/envs/td-bot/chromedriver') as driver:
+            return auth.client_from_login_flow(driver, config.api_key, config.redirect_uri, config.token_path)
+key = authenticate()
 
-endpoint = 'https://api.tdameritrade.com/v1/marketdata/chains'
-
-response = requests.get(endpoint,
-                        params={'apikey': config.api_key,
+response = requests.get('https://api.tdameritrade.com/v1/marketdata/chains',
+                        params={
+                                'apikey': config.api_key,
                                 'contractType': 'call',
                                 'symbol': 'AAPL',
                                 'strikeCount': '2',
                                 'includeQuotes': True
-                                },
-                        headers={'Bearer': f'{key}'})
+                                }, headers={'Bearer': f'{key}'})
+
 data = json.loads(json.dumps(response.json()))
 
 pp = pprint.PrettyPrinter(indent=1)
