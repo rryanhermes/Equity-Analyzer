dbbb5bd4cf01204a638e0b3e958b3e10e643982b
rryanhermes <106575167+rryanhermes@users.noreply.github.com>
2023-04-09 01:43:52 -0500

    test

--- history.py
+++ history.py
@@ -3,7 +3,7 @@ import config
 import json
 from datetime import datetime
 
-key, api_key, symbol = config.key, config.api_key, settings.symbol
+key, api_key, symbol = config.key, config.api_key, 'AAPL'
 response = requests.get(f'https://api.tdameritrade.com/v1/marketdata/{symbol.upper()}/pricehistory',
                         params={
                             'apikey': api_key,
@@ -18,3 +18,4 @@ historical_data = []
 for day in history['candles']:
     historical_data.append([datetime.fromtimestamp(int(str(day['datetime'])[0:10])).strftime('%m-%d'), day['close']])
 
+print(historical_data)
