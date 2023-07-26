43204576559fd5fd660b7b83b1742e51ebdaae81
rryanhermes <106575167+rryanhermes@users.noreply.github.com>
2023-07-26 09:54:36 -0500

    made data collector work

--- datacollector.py
+++ datacollector.py
@@ -1,78 +1,54 @@
 import csv
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 import resources.config as config
-import pprint
-import requests
 
 ticker = 'aapl'
-key, api_key = config.key, config.api_key
-old_price, old_high, old_low, old_open, old_vol = 0, 0, 0, 0, 0
+buffer_size = 10
+
+key, api_key, data_buffer = config.key, config.api_key, []
+
+def streamData():
+    while True:
+        try:
+            response = key.get_quotes(f'{ticker.upper()}').json()[f'{ticker.upper()}']
+            return response
+        except KeyError:
+            continue
 
 def streamTime():
     return datetime.now().strftime('%m-%d %H:%M:%S:%f')
+def streamPrice(response):
+    return response['regularMarketLastPrice']
+def streamVolume(response):
+    return response['regularMarketLastSize']
+def streamOpen(response):
+    return response['openPrice']
+def streamHigh(response):
+    return response['highPrice']
+def streamLow(response):
+    return response['lowPrice']
 
-def streamPrice():
-    while True:
-        try:
-            current_price = key.get_quotes(f'{ticker.upper()}').json()[f'{ticker.upper()}']['regularMarketLastPrice']
-            old_price = current_price
-            return current_price
-        except KeyError:
-            return old_price
+def prepareRow(response):
+    return [streamTime(), streamPrice(response), streamVolume(response), streamOpen(response), streamHigh(response), streamLow(response)]
+def flush_buffered_data(file, data_buffer):
+    with open(file, 'a+', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(data_buffer)
+    data_buffer.clear()
 
-def streamHigh():
-    while True:
-        try:
-            current_high = key.get_quotes(f'{ticker.upper()}').json()[f'{ticker.upper()}']['highPrice']
-            old_high = current_high
-            return current_high
-        except KeyError:
-            return old_high
+for i in range(10800):
+    start = time.time()
 
-def streamLow():
-    while True:
-        try:
-            current_low = key.get_quotes(f'{ticker.upper()}').json()[f'{ticker.upper()}']['lowPrice']
-            old_low = current_low
-            return current_low
-        except KeyError:
-            return old_low
-        
-def streamOpen():
-    while True:
-        try:
-            current_open = key.get_quotes(f'{ticker.upper()}').json()[f'{ticker.upper()}']['openPrice']
-            old_open = current_open
-            return current_open
-        except KeyError:
-            return old_open
-        
-def streamVolume():
-    while True:
-        try:
-            current_vol = key.get_quotes(f'{ticker.upper()}').json()[f'{ticker.upper()}']['regularMarketLastSize']
-            old_vol = current_vol
-            return current_vol
-        except KeyError:
-            return old_vol
+    row = prepareRow(streamData())
+    data_buffer.append(row)
+    print(f"{i+1}: {row}")
 
-def prepareRow():
-    return [streamTime(), streamPrice(), streamVolume(), streamOpen(), streamHigh(), streamLow()]
+    if len(data_buffer) >= buffer_size: flush_buffered_data('data.csv', data_buffer)
 
-with open('data.csv', 'a+', newline='') as file: 
-    writer = csv.writer(file)
-    for i in range(5):
-        start = time.time()
+    try: time.sleep(1 - (time.time() - start))
+    except ValueError: continue
 
-        # print(streamTime())
-        # print(streamPrice())
-        # print(streamHigh())
-        # print(streamLow())
-        # print(streamOpen())
-        # print(streamVolume())
+if data_buffer: flush_buffered_data('data.csv', data_buffer)
 
-        writer.writerow(prepareRow())
-        time.sleep(1 - (time.time() - start))
-        
-    print('finished')
+print('finished')
