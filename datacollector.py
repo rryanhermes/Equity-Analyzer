1f947d4093ca46a3999da1e3372c4594d9a1f6c2
Ryan Hermes <rryanhermes@gmail.com>
2024-02-02 16:56:38 -0600

    more data ig

--- datacollector.py
+++ datacollector.py
@@ -3,11 +3,31 @@ import pandas as pd
 from datetime import datetime
 from config import apikey
 
-date = '2024-01-09'
+# for i in range(1, 30):
+#     date = f'2024-01-{i}'
+#     ticker = 'AAPL'
+#     apikey = apikey
+
+#     # Get dataconda
+#     url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/second/{date}/{date}?adjusted=true&sort=asc&limit=50000&apiKey={apikey}'
+#     response = requests.get(url).json()
+#     results = response.get('results', [])
+#     data = pd.DataFrame(results)
+
+#     # Data prep
+#     data['simpletime'] = data.index + 1
+#     data['date'] = data['t'].apply(lambda x: datetime.fromtimestamp(x / 1000))
+#     data['vwap'] = (data['vw'] * data['v']).cumsum() / data['v'].cumsum()
+#     data = data.rename(columns={'v': 'volume', 'o': 'open', 'c': 'close', 'h': 'high', 'l': 'low', 'n': 'num_trades'})
+
+#     # Save
+#     data.to_csv(f'{ticker}/{ticker} {date}.csv', index=False)
+
+date = f'2024-01-26'
 ticker = 'AAPL'
 apikey = apikey
 
-# Get data
+# Get dataconda
 url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/second/{date}/{date}?adjusted=true&sort=asc&limit=50000&apiKey={apikey}'
 response = requests.get(url).json()
 results = response.get('results', [])
--- workspace.py
+++ workspace.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('data/SNOW 2024-01-09.csv')
+data = pd.read_csv('data/AAPL 2024-01-19.csv')
 
 plt.plot(data['simpletime'], data['close'])
 plt.plot(data['simpletime'], data['vwap'])
