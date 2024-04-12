124f175e6fb5d034a77c355f614514b1161cb0d9
Ryan Hermes <rryanhermes@gmail.com>
2024-04-12 18:58:26 -0500

    idk

--- workspace.py
+++ workspace.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-data = pd.read_csv('data/AAPL 2024-01-19.csv')
+data = pd.read_csv('data/AAPL 2024-01-24.csv')
+
+gap = data['close'] - data['vwap']
 
 plt.plot(data['simpletime'], data['close'])
 plt.plot(data['simpletime'], data['vwap'])
+
+plt.show()
+
+plt.plot(data['simpletime'], gap)
+plt.axhline(y=0, color='black', linestyle='--')
+
 plt.show()
